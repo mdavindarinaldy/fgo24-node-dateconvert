023908ad1d8cc5f746ff23b979e16839454ef433
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-23 14:04:49 +0700

    fixes via eslint

--- index.js
+++ index.js
@@ -2,38 +2,38 @@ import moment from "moment";
 import readline from "readline";
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout
+});
 
 // Menggunakan external packages moment
 function main() {
-    rl.question('Masukkan tanggal : ', function(input) {
-        if (moment(input,'DD-MM-YYYY').isValid()) {
-            console.log(moment(input,'DD-MM-YYYY').format('DD/MM/YYYY'))
-            rl.close()
-        }
-        else { 
-            console.log("Format tanggal salah")
-            main()
-        }
-    })
+  rl.question('Masukkan tanggal : ', function(input) {
+    if (moment(input,'DD-MM-YYYY').isValid()) {
+      console.log(moment(input,'DD-MM-YYYY').format('DD/MM/YYYY'));
+      rl.close();
+    }
+    else { 
+      console.log("Format tanggal salah");
+      main();
+    }
+  });
 }
 
 // Tanpa external package
 function main2() {
-    rl.question('Masukkan tanggal : ', function(input) {
-        let temp = new Date(input.split('-').reverse().join('-'))
-        if (temp=='Invalid Date') {
-            console.log("Format tanggal salah")
-            main2()
-        } else {
-            let newDate = temp.getDate()+'/'+temp.getMonth()+'/'+temp.getFullYear()
-            console.log(newDate)
-            rl.close()
-        }
-    })
+  rl.question('Masukkan tanggal : ', function(input) {
+    let temp = new Date(input.split('-').reverse().join('-'));
+    if (temp==='Invalid Date') {
+      console.log("Format tanggal salah");
+      main2();
+    } else {
+      let newDate = temp.getDate()+'/'+temp.getMonth()+'/'+temp.getFullYear();
+      console.log(newDate);
+      rl.close();
+    }
+  });
 }
 
-// main() // Menggunakan external package moment
-main2() // Tanpa external package
+main(); // Menggunakan external package moment
+main2(); // Tanpa external package
