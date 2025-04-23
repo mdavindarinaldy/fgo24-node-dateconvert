e5ce78c19a05d4ca080892a38ade1eef72676161
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-23 15:14:29 +0700

    add error handling

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
 import moment from "moment";
-import readline from "readline";
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+import readline from "readline/promises";
 
 // Menggunakan external packages moment
-function main() {
-  rl.question('Masukkan tanggal : ', function(input) {
-    if (moment(input,'DD-MM-YYYY').isValid()) {
-      console.log(moment(input,'DD-MM-YYYY').format('DD/MM/YYYY'));
-      rl.close();
+async function main() {
+    try {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        const input = await rl.question('Masukkan tanggal : ')
+        // rl.question('Masukkan tanggal : ', function(input) {
+        if (moment(input,'DD-MM-YYYY').isValid()) {
+          console.log(moment(input,'DD-MM-YYYY').format('DD/MM/YYYY'));
+          rl.close();
+        }
+        else { 
+          let err = new Error("Gagal");
+          throw err;
+          // console.log("Format tanggal salah");
+          // main();
+        }
+    //   });
+    } catch(err) {
+        if(err.message === "Gagal") {
+            console.log("Format tanggal salah");
+        }
     }
-    else { 
-      console.log("Format tanggal salah");
-      main();
-    }
-  });
 }
 
 // Tanpa external package
-function main2() {
-  rl.question('Masukkan tanggal : ', function(input) {
-    let temp = new Date(input.split('-').reverse().join('-'));
-    if (temp==='Invalid Date') {
-      console.log("Format tanggal salah");
-      main2();
-    } else {
-      let newDate = temp.getDate()+'/'+temp.getMonth()+'/'+temp.getFullYear();
-      console.log(newDate);
-      rl.close();
-    }
-  });
-}
+// function main2() {
+//   rl.question('Masukkan tanggal : ', function(input) {
+//     let temp = new Date(input.split('-').reverse().join('-'));
+//     if (temp==='Invalid Date') {
+//     const err = new Error("Gagal");
+//     throw err;
+//     } else {
+//       let newDate = temp.getDate()+'/'+temp.getMonth()+'/'+temp.getFullYear();
+//       console.log(newDate);
+//       rl.close();
+//     }
+//   });
+// }
 
-main(); // Menggunakan external package moment
-main2(); // Tanpa external package
+// try {
+//   main(); // Menggunakan external package moment
+//   // main2(); // Tanpa external package
+// } catch(err) {
+//   if(err.message === "Gagal") {
+//     console.log("Format tanggal salah");
+//   }
+// }
+
+main();
