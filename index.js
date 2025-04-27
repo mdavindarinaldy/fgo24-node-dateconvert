7297fc532e02582d03ecfd101c2ac9686e9d91e8
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-27 11:37:40 +0700

    feat: check if the date is valid (w/o moment)

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 import moment from "moment";
 import readline from "readline/promises";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 // Menggunakan external packages moment
 async function main() {
   try {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
     const input = await rl.question('Masukkan tanggal : ');
-    // rl.question('Masukkan tanggal : ', function(input) {
     if (moment(input,'DD-MM-YYYY').isValid()) {
       console.log(moment(input,'DD-MM-YYYY').format('DD/MM/YYYY'));
       rl.close();
@@ -18,10 +17,7 @@ async function main() {
       let err = new Error("Gagal");
       rl.close();
       throw err;
-      // console.log("Format tanggal salah");
-      // main();
     }
-    //   });
   } catch(err) {
     if(err.message === "Gagal") {
       console.log("Format tanggal salah");
@@ -30,27 +26,32 @@ async function main() {
 }
 
 // Tanpa external package
-// function main2() {
-//   rl.question('Masukkan tanggal : ', function(input) {
-//     let temp = new Date(input.split('-').reverse().join('-'));
-//     if (temp==='Invalid Date') {
-//     const err = new Error("Gagal");
-//     throw err;
+// async function main2() {
+//   try {
+//     const input = await rl.question('Masukkan tanggal : ');
+//     const [day, month, year] = input.split('-');
+//     if (!isNaN(day) || !isNaN(month) || !isNaN(year)) {
+//       let newDate = new Date(`${year}-${month}-${day}`);
+//       if (newDate==='Invalid Date') {
+//         const err = new Error("Gagal");
+//         throw err;
+//       } else {
+//         newDate = `${day}/${month}/${year}`;
+//         console.log(newDate);
+//         rl.close();
+//       }
 //     } else {
-//       let newDate = temp.getDate()+'/'+temp.getMonth()+'/'+temp.getFullYear();
-//       console.log(newDate);
+//       const err = new Error("Gagal");
+//       throw err;
+//     }
+//   } catch (err) {
+//     if(err.message === "Gagal") {
+//       console.log("Format tanggal salah");
 //       rl.close();
 //     }
-//   });
-// }
-
-// try {
-//   main(); // Menggunakan external package moment
-//   // main2(); // Tanpa external package
-// } catch(err) {
-//   if(err.message === "Gagal") {
-//     console.log("Format tanggal salah");
 //   }
 // }
 
-main();
+
+main(); // Menggunakan external package moment
+// main2(); // Tanpa external package
